b4640815e02bb5e01dd2a6970f4fac5a805b8823
ffcs <myoldman@163.com>
2012-11-07 11:04:20 +0800

    添加注册功能

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   include Account
 
   devise :database_authenticatable, :token_authenticatable, :lockable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :registerable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :bio, :name,
                   :skype, :linkedin, :twitter, :dark_scheme, :theme_id, :force_random_password,
